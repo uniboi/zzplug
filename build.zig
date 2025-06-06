987884906cae11bc73096b29aa03ba91a61a9fc2
uniboi <kami.0.katze@gmail.com>
2025-06-06 11:54:13 +0200

    build docs for root module instead of test module

--- build.zig
+++ build.zig
@@ -4,10 +4,10 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const zzplug = b.addModule("zzplug", .{ .root_source_file = b.path("./src/root.zig") });
+    const zzplug = b.addModule("zzplug", .{ .root_source_file = b.path("./src/zzplug.zig") });
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/zzplug.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -16,18 +16,32 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
 
-    const docs_step = b.step("docs", "Emit documentation");
-
-    const docs_install_dir = b.addInstallDirectory(.{
-        .install_dir = .prefix,
-        .install_subdir = "docs",
-        .source_dir = lib_unit_tests.getEmittedDocs(),
-    });
-    docs_step.dependOn(&docs_install_dir.step);
+    addDocs(b, target, optimize);
 
     addExample(b, "hello_world", "log hello world when the plugin is loaded", zzplug, target, optimize);
     addExample(b, "hello_squirrel", "register a squirrel function for all SQVMs", zzplug, target, optimize);
     addExample(b, "print_sequence", "register a sqvm function to print the current playing animation of an entity", zzplug, target, optimize);
+}
+
+fn addDocs(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+) void {
+    const shared_lib = b.addSharedLibrary(.{
+        .name = "zzplug",
+        .root_source_file = b.path("src/zzplug.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const docs_step = b.step("docs", "Emit documentation");
+    const docs_install_dir = b.addInstallDirectory(.{
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+        .source_dir = shared_lib.getEmittedDocs(),
+    });
+    docs_step.dependOn(&docs_install_dir.step);
 }
 
 fn addExample(
