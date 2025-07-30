b842c1a294e7dfb7bb160c4af63745ce0011489f
uniboi <kami.0.katze@gmail.com>
2025-07-30 18:12:12 +0200

    fix callconv in example

--- examples/hello_squirrel.zig
+++ examples/hello_squirrel.zig
@@ -2,7 +2,7 @@
 //! Try it out with `script print(Increment(1))` in the MP lobby (requires sv_cheats)
 //! This example shows the basics of squirrel objects and nativeclosures
 
-fn init(_: *const PluginCallbacks.Instance, _: HMODULE, _: *const PluginCallbacks.InitData, _: bool) callconv(.C) void {}
+fn init(_: *const PluginCallbacks.Instance, _: HMODULE, _: *const PluginCallbacks.InitData, _: bool) callconv(.c) void {}
 
 fn onSqvmCreated(_: *const PluginCallbacks.Instance, c_sqvm: *sq.C_SQVM) callconv(.c) void {
     // onSqvmCreated is called for every SQVM, regardless of origin
