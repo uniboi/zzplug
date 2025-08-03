3a7852dab77710104ac43a7e8ebe6e22b4cc400a
uniboi <kami.0.katze@gmail.com>
2025-08-03 12:04:27 +0200

    comptime offset

--- src/abi.zig
+++ src/abi.zig
@@ -23,7 +23,7 @@ pub fn assertSize(comptime T: type, comptime size: comptime_int) void {
     );
 }
 
-pub fn assertOffset(comptime T: type, comptime field: []const u8, offset: usize) void {
+pub fn assertOffset(comptime T: type, comptime field: []const u8, comptime offset: usize) void {
     if (@offsetOf(T, field) != offset) @compileError(
         std.fmt.comptimePrint(
             "Offset {s}.{s} is incorrect. Expected {2d} (0x{2X}), got {3d} (0x{3X})",
