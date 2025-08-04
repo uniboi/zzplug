09102e3e9d97776949fa9cf7b0ab9f040a227b9f
uniboi <kami.0.katze@gmail.com>
2025-08-04 17:20:07 +0200

    vector isZero

--- src/math/vector.zig
+++ src/math/vector.zig
@@ -3,6 +3,10 @@ pub const Vector3 = extern struct {
     y: f32,
     z: f32,
 
+    pub fn isZero(v: Vector3) bool {
+        return v.x == 0 and v.y == 0 and v.z == 0;
+    }
+
     test {
         comptime abi.assertSize(@This(), 0xc);
     }
@@ -11,6 +15,10 @@ pub const Vector3 = extern struct {
 pub const Vector2 = extern struct {
     x: f32,
     y: f32,
+
+    pub fn isZero(v: Vector2) bool {
+        return v.x == 0 and v.y == 0;
+    }
 
     test {
         comptime abi.assertSize(@This(), 0x8);
@@ -22,6 +30,10 @@ pub const Quaternion = extern struct {
     y: f32,
     z: f32,
     w: f32,
+
+    pub fn isZero(v: Quaternion) bool {
+        return v.x == 0 and v.y == 0 and v.z == 0 and v.w == 0;
+    }
 };
 
 const abi = @import("../abi.zig");
