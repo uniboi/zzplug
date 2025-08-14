ae2a32c65eef250507bd9048b1e5dfeb31c2e321
uniboi <kami.0.katze@gmail.com>
2025-08-14 21:23:30 +0200

    delete assertInheritance

--- src/abi.zig
+++ src/abi.zig
@@ -1,21 +1,5 @@
 pub const cpp = @import("./abi/cpp.zig");
 
-pub fn assertInheritance(comptime Child: type, comptime Parent: type) void {
-    const child_info = @typeInfo(Child);
-    const parent_info = @typeInfo(Parent);
-
-    if (child_info != .@"struct" or parent_info != .@"struct") @compileError("Parent and Child must be structs");
-    for (child_info.@"struct".fields[0..parent_info.@"struct".fields.len], parent_info.@"struct".fields) |child_field, parent_field| {
-        if (@offsetOf(Child, child_field.name) != @offsetOf(Parent, parent_field.name)) @compileError("Inherited member has different offset");
-        if (std.mem.eql(u8, child_field.name, parent_field.name) == false) @compileError("Inherited member has different name. Expected " ++ parent_field.name ++ ", got " ++ child_field.name);
-        if (!std.mem.eql(u8, child_field.name, "vtable") and // TODO check vtables are properly inherited
-            child_field.type != parent_field.type)
-        {
-            @compileError("Inherited member " ++ child_field.name ++ " has different Type");
-        }
-    }
-}
-
 pub fn assertSize(comptime T: type, comptime size: comptime_int) void {
     if (@sizeOf(T) != size) @compileError(
         std.fmt.comptimePrint(
