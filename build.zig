14d3341707556dff0f4e6d23dbb70ae7f0e02d1d
uniboi <kami.0.katze@gmail.com>
2025-08-20 19:31:40 +0200

    create private example modules

--- build.zig
+++ build.zig
@@ -53,11 +53,12 @@ fn addExample(
     zzplug: *std.Build.Module,
     target: std.Build.ResolvedTarget,
 ) void {
-    const example_module = b.addModule("example." ++ name, .{ .root_source_file = b.path("examples/" ++ name ++ ".zig"), .target = target });
-
     const example = b.addLibrary(.{
         .name = name ++ "_example",
-        .root_module = example_module,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("examples/" ++ name ++ ".zig"),
+            .target = target,
+        }),
         .linkage = .dynamic,
     });
     example.root_module.addImport("zzplug", zzplug);
