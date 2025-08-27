e979539f1188c5f056f2a581b5c5246a5d817916
uniboi <kami.0.katze@gmail.com>
2025-08-27 16:35:34 +0200

    more convar things

--- src/valve/tier1.zig
+++ src/valve/tier1.zig
@@ -13,14 +13,124 @@ pub const ConCommandBase = extern struct {
     m_bRegistered: bool,
     m_pszName: [*:0]const u8,
     m_pszHelpString: [*:0]const u8,
-    m_nFlags: u32,
+    m_nFlags: Flags,
 
     pub const VTable = extern struct {
-        // ...
+        destructor: *const fn (*ConCommandBase, dealloc: bool) callconv(.c) *ConCommandBase,
+        is_flag_set: *const fn (*ConCommandBase, flags: u32) callconv(.c) bool,
+        add_flags: *const fn (*ConCommandBase, flags: u32) callconv(.c) void,
+        remove_flags: *const fn (*ConCommandBase, flags: u32) callconv(.c) void,
+        get_flags: *const fn (*ConCommandBase, flags: u32) callconv(.c) void,
+        get_name: *const fn (*ConCommandBase) callconv(.c) [*:0]const u8,
+        get_help_text: *const fn (*ConCommandBase) callconv(.c) [*:0]const u8,
+        is_registered: *const fn (*ConCommandBase) callconv(.c) bool,
+        get_dll_identifier: *const fn (*ConCommandBase) callconv(.c) CVarDLLIdentifier,
+        create: *const fn (*ConCommandBase, name: [*:0]const u8, help_text: [*:0]const u8, flags: Flags) callconv(.c) void,
+        init: *const fn (*ConCommandBase) callconv(.c) void,
     };
 
     pub const Flags = packed struct(u32) {
-        _: u32 = 0, // TODO: figure out flags
+        /// FCVAR_UNREGISTERED
+        unregistered: bool,
+
+        /// FCVAR_DEVELOPMENTONLY
+        development_only: bool,
+
+        /// FCVAR_GAMEDLL
+        game_dll: bool,
+
+        /// FCVAR_CLIENTDLL
+        client_dll: bool,
+
+        /// FCVAR_HIDDEN
+        hidden: bool,
+
+        /// FCVAR_PROTECTED
+        /// ConVar only
+        protected: bool,
+
+        /// FCVAR_SPONLY
+        sp_only: bool,
+
+        /// FCVAR_ARCHIVE
+        archive: bool,
+
+        /// FCVAR_NOTIFY
+        notify: bool,
+
+        /// FCVAR_USERINFO
+        user_info: bool,
+
+        /// Means different things for ConVars and ConCommands
+        ambiguous: packed union {
+            /// FCVAR_PRINTABLEONLY
+            ///
+            /// ConVars only
+            printable_only: bool,
+
+            /// FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS
+            ///
+            /// ConCommands only
+            game_dll_for_remote_clients: bool,
+        },
+
+        /// FCVAR_UNLOGGED
+        unlogged: bool,
+
+        /// FCVAR_NEVER_AS_STRING
+        never_as_string: bool,
+
+        /// FCVAR_REPLICATED
+        replicated: bool,
+
+        /// FCVAR_CHEAT
+        cheat: bool,
+
+        /// FCVAR_SS
+        splitscreen: bool,
+
+        /// FCVAR_DEMO
+        demo: bool,
+
+        /// FCVAR_DONTRECORD
+        dont_record: bool,
+
+        /// FCVAR_SS_ADDED
+        splitscreen_added: bool,
+
+        /// FCVAR_RELEASE
+        release: bool,
+
+        /// FCVAR_RELOAD_MATERIALS
+        reload_materials: bool,
+
+        /// FCVAR_RELOAD_TEXTURES
+        reload_textures: bool,
+
+        /// FCVAR_NOT_CONNECTED
+        not_connected: bool,
+
+        /// FCVAR_MATERIAL_SYSTEM_THREAD
+        material_system_thread: bool,
+
+        /// FCVAR_ARCHIVE_GAMECONSOLE
+        archive_game_console: bool,
+
+        /// FCVAR_ACCESSIBLE_FROM_THREADS
+        accessible_from_threads: bool,
+
+        _gap_26: u2,
+
+        /// FCVAR_SERVER_CAN_EXECUTE
+        server_can_execute: bool,
+
+        /// FCVAR_SERVER_CANNOT_QUERY
+        server_cannot_query: bool,
+
+        /// FCVAR_CLIENTCMD_CAN_EXECUTE
+        clientcmd_can_execute: bool,
+
+        _gap_31: u1,
     };
 
     test "layout" {
@@ -37,10 +147,20 @@ pub const ConCommand = extern struct {
     vtable: *VTable,
     ConCommandBase: abi.cpp.Inherit(ConCommandBase),
 
-    m_fnSupplementalFinishCallback: *anyopaque,
-    m_fnSupplementalCallback: *anyopaque,
-    m_pCommandCallback: *const CommandCallback,
-    m_pCompletionCallback: *const CommandCompletionCallback,
+    m_fnSupplementalFinishCallback: *CommandSupplementalCompletionCallback,
+    m_fnSupplementalCallback: ?*CommandSupplementalCallback,
+
+    command_callback: extern union {
+        m_fnCommandCallbackV1: *const fn() callconv(.c) void,
+        m_fnCommandCallback: *const CommandCallback,
+        m_pCommandCallback: *ICommandCallback,
+    },
+
+    completion_callback: extern union {
+        m_pCompletionCallback: *const CommandCompletionCallback,
+        m_pCommandCompletionCallback: *ICommandCompletionCallback,
+    },
+
     m_nCallbackFlags: u32,
 
     /// Create a ConCommand.
@@ -64,10 +184,6 @@ pub const ConCommand = extern struct {
         );
     }
 
-    pub const VTable = extern struct {
-        // ...
-    };
-
     pub const CallbackFlags = packed struct(u32) {
         m_bHasCompletionCallback: bool,
         m_bUsingNewCommandCallback: bool,
@@ -75,11 +191,34 @@ pub const ConCommand = extern struct {
         _: u29 = 0,
     };
 
+    /// FnCommandSupplementalFinishCallback_t
+    const CommandSupplementalCompletionCallback = *const fn () callconv(.c) void;
+    /// FnCommandSupplementalCallback_t
+    /// missing supplemental completion callback arg?
+    const CommandSupplementalCallback = *const fn (command: *const CCommand) void;
+
     pub const CommandCallback = fn (command: *const CCommand) callconv(.c) void;
     pub const CommandCompletionCallback = fn (partial: [*:0]const u8, commands: *const [command_completion_max_items][command_completion_item_length]u8) callconv(.c) i32;
 
+    pub const ICommandCallback = extern struct {
+        vtable: extern struct {
+            callback: *const fn(command: *const CCommand) callconv(.c) void,
+        },
+    };
+
+    pub const ICommandCompletionCallback = extern struct {
+        vtable: extern struct {
+            callback: *const fn(pPartial: [*:0]const u8, commands: *CUtlVector(CUtlString)) callconv(.c) void,
+        },
+    };
+
     const command_completion_max_items = 64;
     const command_completion_item_length = 128;
+
+    const VTable = extern struct {
+        // ...
+    };
+
 
     test "layout" {
         abi.assertSize(@This(), 0x58);
@@ -87,12 +226,13 @@ pub const ConCommand = extern struct {
 };
 
 pub const CCommand = extern struct {
-    // TODO: how sure are we these are 64 bit and not 32?
-    m_nArgc: i64,
-    m_nArgv0Size: i64,
+    m_nArgc: i32,
+    m_nArgv0Size: i32,
     m_pArgSBuffer: [command_max_length]u8,
     m_pArgvBuffer: [command_max_length]u8,
     m_ppArgv: *const [command_max_argc]u8,
+
+    // TODO: cmd_source_t m_source missing?
 
     const command_max_argc = 64;
     const command_max_length = 512;
@@ -109,7 +249,7 @@ pub const IConVar = extern struct {
 pub const ConVar = extern struct {
     vtable: *const VTable,
     ConCommandBase: abi.cpp.Inherit(ConCommandBase),
-    IConVar: IConVar, // should technically be abi.Inherit(IConVar) and the vtable embedded directly
+    IConVar: IConVar, // should technically be abi.cpp.Inherit(IConVar) and the vtable embedded directly
     m_pParent: *const ConCommandBase,
     m_pszDefaultValue: [*:0]const u8,
     m_Value: Value,
@@ -117,7 +257,7 @@ pub const ConVar = extern struct {
     m_fMinVal: f32,
     m_bHasMax: bool,
     m_fMaxVal: f32,
-    m_fnChangeCallbacks: CUtlVector(ChangeCallback, CUtlMemory(ChangeCallback, i32)),
+    m_fnChangeCallbacks: CUtlVector(ChangeCallback),
 
     /// CVValue_t
     pub const Value = extern struct {
@@ -161,7 +301,15 @@ pub fn CUtlMemory(T: type, I: type) type {
     };
 }
 
-pub fn CUtlVector(T: type, CAllocator: type) type {
+/// CUtlVector<T>
+///
+/// Use CUtlVector_Allocator for CUtlVector<T, A>
+pub fn CUtlVector(T: type) type {
+    return CUtlVector_Allocator(T, CUtlMemory(T, i32));
+}
+
+/// CUtlVector<T, A>
+pub fn CUtlVector_Allocator(T: type, CAllocator: type) type {
     return extern struct {
         m_Memory: CAllocator,
         m_Size: i32,
@@ -172,6 +320,17 @@ pub fn CUtlVector(T: type, CAllocator: type) type {
         }
     };
 }
+
+pub const CUtlBinaryBlock = extern struct {
+    m_Memory: CUtlMemory(u8, i32),
+    m_nActualLength: i32,
+};
+
+pub const CUtlString = extern struct {
+    m_Storage: CUtlBinaryBlock,
+};
+
+pub const CVarDLLIdentifier = enum(i32) { _ };
 
 const abi = @import("../abi.zig");
 const std = @import("std");
